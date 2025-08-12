38e61c88fd9342e657cc4540cb142a5a1bd1f21f
snettem2 <snettem2@gitam.in>
2025-08-12 11:41:32 +0530

    Create version2_keyboard_input.py

---
+++ version2_keyboard_input.py
@@ -0,0 +1,5 @@
+a = float(input("Enter coefficient a: "))
+b = float(input("Enter coefficient b: "))
+c = float(input("Enter coefficient c: "))
+t = float(input("Enter time t (hour/day): "))
+T = a * t**2 + b * t + c
